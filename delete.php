333550c1c9b04fbe1a6ba221692f9c21180456a7
skibidi1945 <aryamilito755@gmail.com>
2025-09-12 13:14:51 +0700

    delete

---
+++ delete.php
@@ -0,0 +1,7 @@
+<?php
+include 'koneksi .php';
+
+$id = $_GET['id'];
+mysqli_query($conn, "DELETE FROM siswa WHERE id='$id'");
+
+echo "<script>alert('Data berhasil dihapus'); window.location='.php';</script>";
